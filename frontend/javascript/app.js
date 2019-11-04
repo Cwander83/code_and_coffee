750df48c75836d33c2438cddbc61d0ddc19abb1c
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2019-11-04 09:29:50 -0500

    node_modules bug fix

--- frontend/javascript/app.js
+++ frontend/javascript/app.js
@@ -1,18 +1,44 @@
 console.log('hello');
 
+
 var map;
 
 
+function place() {
+  fetch(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${google_api}`, {
+        mode: 'no-cors'
+      }
+    )
+     .then(
+         function (response) {
+           if (response.status !== 200) {
+             console.log('Looks like there was a problem. Status Code: ' +
+               response.status);
+             return;
+           }
 
+           // Examine the text in the response
+           response.json().then(function (data) {
+             console.log(data);
+           });
+         }
+       )
+       .catch(function (err) {
+         console.log('Fetch Error :-S', err);
+       });
 
-function initMap() {
-  var sydeny = { lat: -34.397, lng: 150.644 };
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: sydeny,
-    zoom: 4,
-  });
-  var marker = new google.maps.Marker({ position: sydeny, map: map });
 }
 
+place();
 
+
+// function initMap() {
+//   var sydeny = { lat: -34.397, lng: 150.644 };
+
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: sydeny,
+//     zoom: 4,
+//   });
+//   var marker = new google.maps.Marker({ position: sydeny, map: map });
+// }
